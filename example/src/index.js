008b99b0876058137d8084febb0a81921e2d1cb3
shenghanqin <shenghanqin@hujiang.com>
2020-01-22 11:23:16 +0800

    update 暂时修改仓库名称2

--- example/src/index.js
+++ example/src/index.js
@@ -6,7 +6,7 @@ import Sync from './Sync'
 import Async from './Async';
 
 ReactDOM.render(<Fragment>
-  <p>项目地址：<a href="https://github.com/shenghanqin/react-address-picker">react-address-picker</a></p>
+  <p>项目地址：<a href="https://github.com/shenghanqin/react-address-picker-cc">react-address-picker-cc</a></p>
   <br />
   <Sync />
   <Async />
